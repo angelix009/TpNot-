d246015b633464aae13fe4ca14a698d7a122b4ce
Alicia <alicia@MacBook-Pro-de-alicia.local>
2023-03-14 18:13:05 +0100

    final app

--- src/view/navbar.js
+++ src/view/navbar.js
@@ -14,18 +14,21 @@ export default {
         const linkService = new LinkService();
         const links = linkService.getLinks();
         this.links = [
-          { label: 'Home', url: links[0] },
-          { label: 'Teams', url: links[1] },
+          { label: 'home', url: links[0] },
+          { label: 'team', url: links[1] },
           { label: 'Results', url: links[2] }
         ];
+      },
+      changePage(page) {
+        this.$emit('change-page', page);
       }
     },
     template: `
         <nav>
             <ul class="menu-pannel">
-            <li  v-for="link in links" :key="link.label">
-                <a :href="link.url">{{ link.label }}</a>
-            </li>
+              <li class="menu-pannel-item" v-for="link in links" @click="changePage(link.label)" :key="link.label">
+                  {{ link.label }}
+              </li>
             </ul>
         </nav>
     `   
